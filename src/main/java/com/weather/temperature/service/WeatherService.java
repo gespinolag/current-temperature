36d0b526691ef178246682098ef76cefcb9d1399
gespinolag <davidespinola1997@gmail.com>
2026-04-02 00:21:54 -0300

    Ajusta resultado devuelto por endpoint
    Se ajusta el resultado devuelto por el endpoint, de xml a json para mayor comodidad. Además, se establece que el valor de la temperatura siempre se devuelva como número decimal. Se ajusta a 30 minutos la llamada al servicio de la DMH y se imprime la hora de dicha ejecución en el log de la aplicación.

--- src/main/java/com/weather/temperature/service/WeatherService.java
+++ src/main/java/com/weather/temperature/service/WeatherService.java
@@ -2,7 +2,8 @@ package com.weather.temperature.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.weather.temperature.model.Temperature;
+import com.weather.temperature.model.TemperatureDTO;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,9 +14,15 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class WeatherService {
     private static final String URL = "https://www.meteorologia.gov.py/emas/data.json";
-    private final ConcurrentHashMap<String, Temperature> temperatureCache = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, TemperatureDTO> temperatureCache = new ConcurrentHashMap<>();
+    private static final String DEFAULT_CODE = "0000086233";
 
-    public Temperature fetchTemperature(String codigo) {
+    @PostConstruct
+    public void init() {
+        fetchTemperature(DEFAULT_CODE);
+    }
+
+    public TemperatureDTO fetchTemperature(String codigo) {
         RestTemplate restTemplate = new RestTemplate();
         try {
             String jsonResponse = restTemplate.getForObject(URL, String.class);
@@ -26,37 +33,33 @@ public class WeatherService {
             if (stationNode.isMissingNode() || stationNode.path("ultima_observacion").path("temperatura_aire").isMissingNode()) {
                 return null;
             }
-            //String valor = stationNode.path("ultima_observacion").path("temperatura_aire").path("valor").asText() + "°";
-            double temperaturaAire = stationNode.path("ultima_observacion").path("temperatura_aire").path("valor").asDouble();
-            //String valor = String.format(Locale.US,"%.1f", temperaturaAire) + "°";
-            String valor;
 
-            valor = String.format(Locale.US, "%.1f", temperaturaAire) + "°";
+            double temperaturaAire = stationNode
+                    .path("ultima_observacion")
+                    .path("temperatura_aire")
+                    .path("valor")
+                    .asDouble();
 
-            /*if (temperaturaAire % 1 == 0) {
-                valor = String.format(Locale.US, "%.0f", temperaturaAire) + "°";
-            } else {
-                valor = String.format(Locale.US, "%.1f", temperaturaAire) + "°";
-            }*/
+            String valor = String.format(Locale.US, "%.1f", temperaturaAire) + "°";
 
-            Temperature temperature = new Temperature(valor);
+            TemperatureDTO temperature = new TemperatureDTO(valor);
             temperatureCache.put(codigo, temperature);
+
         } catch (IOException e) {
             return null;
         }
         return temperatureCache.get(codigo);
     }
 
-    public Temperature getTemperatureByCode(String codigo) {
-        //return temperatureCache.getOrDefault(codigo, fetchTemperature(codigo));
-        Temperature temperature = temperatureCache.get(codigo);
+    public TemperatureDTO getTemperatureByCode(String codigo) {
+        TemperatureDTO temperature = temperatureCache.get(codigo);
         if (temperature == null) {
             return fetchTemperature(codigo);
         }
         return temperature;
     }
 
-    public Iterable<String> getAllCachedCodigos() {
+    public Iterable<String> getAllCachedCodes() {
         return temperatureCache.keySet();
     }
 }
